d3d79a2ae84c6cc419ac53cd67b1a4f3ad4258eb
Steve Clarke <steve@sevenview.ca>
2020-12-03 19:08:46 -0330

    Update rails_helper.rb
    Added a comment to the `use_transactional_fixtures` config option to clarify that RSpec does not do any database management to avoid confusion about where to look when the database is not acting as expected when running specs.

--- lib/generators/rspec/install/templates/spec/rails_helper.rb
+++ lib/generators/rspec/install/templates/spec/rails_helper.rb
@@ -39,7 +39,8 @@ RSpec.configure do |config|
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
-  # instead of true.
+  # instead of true. This delegates to Rails and RSpec itself does no handling
+  # of your database.
   config.use_transactional_fixtures = true
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
